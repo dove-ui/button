9be7f5cb3d00a9fc0e03159b85e46bc1511f227c
ygl <lanyueos@qq.com>
2020-01-04 19:28:06 +0800

    feat: 增加一些属性

--- src/button/button.vue
+++ src/button/button.vue
@@ -1,10 +1,35 @@
 <template>
-  <button class="vc-button"><slot></slot></button>
+  <button class="vc-button"
+    :disabled="disabled"
+    :class="[
+      type && 'vc-button-type--' + type,
+      size && 'vc-button-size--' + size,
+      {
+        'vc-button--plain': plain,
+        'vc-button--round': round
+      }
+    ]"
+    :type="nativeType"
+    @click="$emit('click')"
+    ><span><slot></slot></span></button>
 </template>
 
 <script>
 export default {
   name: 'VcButton',
-  props: {}
+  props: {
+    type: {
+      type: String,
+      default: 'default'
+    }, // default，info，primary，warning，danger，success, text
+    size: String, // max, large, 默认，medium，samll, mini
+    plain: Boolean,
+    round: Boolean,
+    disabled: Boolean,
+    nativeType: {
+      type: String,
+      default: 'button'
+    } // 原生type button / submit / reset
+  }
 }
 </script>
