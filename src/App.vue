6f154818ae46d1c694364a2ab934c9e4cfc15487
ygl <lanyueos@qq.com>
2020-01-04 19:28:33 +0800

    docs: 添加例子

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,50 @@
 <template>
   <div id="app">
-    <vc-button>Button</vc-button>
+    <div class="demo-block">
+      <vc-button @click="handleClick">Default</vc-button>
+      <vc-button type="info">Info</vc-button>
+      <vc-button type="primary">Primary</vc-button>
+      <vc-button type="warning">Warning</vc-button>
+      <vc-button type="danger">Danger</vc-button>
+      <vc-button type="success">Success</vc-button>
+      <vc-button type="text">Text</vc-button>
+    </div>
+
+    <div class="demo-block">
+      <vc-button size="large">Large</vc-button>
+      <vc-button >Default</vc-button>
+      <vc-button size="medium">Medium</vc-button>
+      <vc-button size="small">Small</vc-button>
+      <vc-button size="mini">mini</vc-button>
+    </div>
+
+    <div class="demo-block">
+      <vc-button plain round>Default</vc-button>
+      <vc-button plain round type="info">Info</vc-button>
+      <vc-button plain round type="primary">Primary</vc-button>
+      <vc-button plain round type="warning">Warning</vc-button>
+      <vc-button plain round type="danger">Danger</vc-button>
+      <vc-button plain round type="success">Success</vc-button>
+    </div>
+
+    <div class="demo-block">
+      <vc-button round>Default</vc-button>
+      <vc-button round type="info">Info</vc-button>
+      <vc-button round type="primary">Primary</vc-button>
+      <vc-button round type="warning">Warning</vc-button>
+      <vc-button round type="danger">Danger</vc-button>
+      <vc-button round type="success">Success</vc-button>
+    </div>
+
+    <div class="demo-block">
+      <vc-button disabled>Default</vc-button>
+      <vc-button disabled type="info">Info</vc-button>
+      <vc-button disabled type="primary">Primary</vc-button>
+      <vc-button disabled type="warning">Warning</vc-button>
+      <vc-button disabled type="danger">Danger</vc-button>
+      <vc-button disabled type="success">Success</vc-button>
+    </div>
+   
   </div>
 </template>
 
@@ -11,7 +55,16 @@ export default {
   name: 'app',
   components: {
     VcButton
+  },
+  methods: {
+    handleClick () {
+      console.log(arguments)
+    }
   }
 }
 </script>
-
+<style>
+  .demo-block {
+    padding: 15px;
+  }
+</style>
